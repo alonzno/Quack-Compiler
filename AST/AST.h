2c147f3f73ff72706658dfaceb0bc5e12776c5f3
alonzno <alonzoa@cs.uoregon.edu>
2018-10-31 20:20:56 -0700

    Add new branch for AST

---
+++ AST/AST.h
@@ -0,0 +1,46 @@
+#ifndef AST_H
+#define AST_H
+
+#include <string>
+#include <sstream>
+#include <vector>
+
+namespace AST {
+    
+    class AST_print_context {
+        public:
+            int indent_;
+            AST_print_context() : indent_{0} {}
+            void indent() { ++indent_; }
+            void dedent() { --indent_; }
+    };
+
+
+    class ASTNode {
+        public:
+            virtual void json(std::ostream &out, AST_print_context &ctx) = 0;
+
+        void json_indent(std::ostream& out, AST_print_context& ctx);
+        void json_head(std::string node_kind, std::ostream& out, AST_print_context& ctx);
+        void json_close(std::ostream& out, AST_print_context& ctx);
+        void json_child(std::string field, ASTNode& child, std::ostream& out, AST_print_context& ctx, char sep=',');
+    };
+    
+    class Block: public ASTNode {
+        private:
+            std::vector<ASTNode *> stmts_;
+        public:
+            void append(ASTNode *stmt) {stmts_.push_back(stmt)}
+            void json(std::ostream &out, unsigned int ident = 0);
+    };
+
+    class Ident: public ASTNode {
+        private:
+            std::string text_;
+        public:
+            Ident(std::string name): text_(name) {}
+    };
+
+}
+
+#endif //AST_H
---
+++ AST/parser.cpp
@@ -0,0 +1,28 @@
+//
+// The parser driver just glues together a parser object
+// and a lexer object.
+//
+
+#include "lex.yy.h"
+#include "Messages.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+    {
+        std::cout << "Usage: ./" 
+            << argv[0] 
+            << " <filename>" 
+            << std::endl;
+    }
+
+    std::ifstream file;
+    file.open(std::string(argv[1]));
+
+    yy::Lexer *lexer  = new yy::Lexer(file);
+    yy::parser *parser = new yy::parser(*lexer, argv[1]);
+    parser->parse();
+}
