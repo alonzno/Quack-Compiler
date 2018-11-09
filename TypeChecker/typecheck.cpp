0fee1a760a421f48cddaa48213f38548bb8a495a
alonzno <alonzoa@cs.uoregon.edu>
2018-11-09 13:08:40 -0800

    Add Class Cycle Checking

--- TypeChecker/typecheck.cpp
+++ TypeChecker/typecheck.cpp
@@ -1,6 +1,10 @@
-#include "typecheck.h"
+#include <stack>
 #include <iostream>
-void TypeChecker::checkClasses() {
+#include <algorithm>
+
+#include "typecheck.h"
+
+bool TypeChecker::checkClasses() {
     AST::Block *classes_ = (AST::Block *)((AST::Block *) (*root_)) -> stmts_[0];
     AST::AST_print_context ctx;   
     int num_classes = classes_ -> stmts_.size();
@@ -11,11 +15,51 @@ void TypeChecker::checkClasses() {
         clazz_ = (AST::Class *) classes_ -> stmts_[i];
         class_name = ((AST::Ident *) clazz_ -> class_name_) -> text_;
         super_name = ((AST::Ident *) clazz_ -> super_name_) -> text_;
+        if (super_name == "") {
+            super_name = "Obj";
+        }
+
         it = class_heir_.find(super_name);
         if (it == class_heir_.end()) {
-            std::vector<std::string> *v = new std::vector<std::string>();
+            std::vector<std::string> v;
             class_heir_.insert(Entry(super_name, v));
         }
-        class_heir_[super_name] -> push_back(class_name);
+        class_heir_[super_name].push_back(class_name);
     }
+
+    //DEBUG
+    for (auto p: class_heir_) {
+        std::cerr << p.first << " ";
+        for (auto v: p.second) {
+            std::cerr << v << " ";
+        }
+        std::cerr << std::endl;
+    }
+
+    //Detect Cycle
+    std::vector<std::string> seen;
+    std::stack<std::string> dfs_stack;
+    std::string curr_node;
+    
+    dfs_stack.push("Obj");
+    while (!dfs_stack.empty()) {
+
+        curr_node = dfs_stack.top();
+        dfs_stack.pop();
+        if (std::find(seen.begin(), seen.end(), curr_node) != seen.end()) {
+            std::cerr << "FALSE" << std::endl; //DEBUGGING
+            return false;
+        }
+        seen.push_back(curr_node);
+        if (class_heir_.find(curr_node) == class_heir_.end()) {
+            continue;
+        }
+        for(auto s: class_heir_[curr_node]) {
+            dfs_stack.push(s);
+        }
+
+    }
+    std::cerr << "TRUE" << std::endl; //DEBUGGING
+    return true;
+
 }
--- TypeChecker/typecheck.h
+++ TypeChecker/typecheck.h
@@ -4,8 +4,8 @@
 #include <AST.h>
 #include <map>
 
-typedef std::pair<std::string, std::vector<std::string> *> Entry;
-typedef std::map<std::string, std::vector<std::string> *> Table;
+typedef std::pair<std::string, std::vector<std::string>> Entry;
+typedef std::map<std::string, std::vector<std::string>> Table;
 
 class TypeChecker
 {
@@ -13,7 +13,7 @@ class TypeChecker
     Table class_heir_;
     public:
         TypeChecker(AST::ASTNode **root): root_(root) {};
-        void checkClasses();
+        bool checkClasses();
 };
 
 #endif
